e38c0a33323a915ebe1f7c7fc0b79896281201ad
s-ong-c <somony9292@gmail.com>
2019-07-07 22:09:26 +0900

    Button working....

--- blog-client/src/components/common/Button.tsx
+++ blog-client/src/components/common/Button.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-const ButtonBlock = styled.div``;
-interface ButtonProps{}
+const ButtonBlock = styled.div<{ theme: string}>`
+  display: flex-inline;
+  padding: 0.5rem 1rem;
+`;
+interface ButtonProps{
+  theme?: string;
+}
 
-const Button: React.SFC<ButtonProps> = props => {
-  return <ButtonBlock />;
+const Button: React.SFC<ButtonProps> = ({theme, children}) => {
+  return <ButtonBlock theme={theme}>{children}</ButtonBlock>;
   };
+
+Button.defaultProps = {
+  theme: 'default', // black button
+};
 
 export default Button;
