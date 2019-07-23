8887d025805b3a83539b8a747bb0163217ed743c
s-ong-c <somony9292@gmail.com>
2019-07-24 00:28:18 +0900

    front UI implement & icons &  left -right

--- blog-client/src/components/main/MainTemplate.tsx
+++ blog-client/src/components/main/MainTemplate.tsx
@@ -1,12 +1,46 @@
 import * as React from 'react';
-import styled from 'styled-components';
+import styled, { StyledComponent } from 'styled-components';
 import PageTemplate from '../base/PageTemplate';
+import { any } from 'prop-types';
+import { breakpoints } from '../../lib/styles/responsive';
 
-const MainTemplateBlock = styled(PageTemplate)``;
-interface MainTemplateProps{}
+const MainTemplateBlock = styled(PageTemplate)`
+main {
+  width: ${breakpoints.xlarge};
+  margin: 0 auto;
+  margin-top: 3.5rem;
+  display: flex;
+  justify-content: space-between;
+}
+`;
+const Left = styled.div`
+  width: 192px;
+`;
+const Main = styled.div`
+  width: 702px;
+`;
+const Right = styled.div`
+  width: 240px;
+`;
 
-const MainTemplate: React.SFC<MainTemplateProps> = props => {
-  return <MainTemplateBlock />;
+type MainTemplateNamespace = {
+  Left: typeof Left;
+  Main: typeof Main;
+  Right: typeof Right;
 };
 
+interface MainTemplateProps{}
+const MainTemplate: React.SFC<MainTemplateProps> & MainTemplateNamespace = ({
+  children,
+}) => {
+  return (
+      <MainTemplateBlock>
+        <main>{children}</main>
+      </MainTemplateBlock>
+    );
+};
+
+MainTemplate.Left = Left;
+MainTemplate.Main = Main;
+MainTemplate.Right = Right;
 export default MainTemplate; 
