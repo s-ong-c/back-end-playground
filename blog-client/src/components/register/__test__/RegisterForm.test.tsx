8f9b55e1528223680b8e060c12c54c239eb57634
s-ong-c <somony9292@gmail.com>
2019-08-15 21:38:49 +0900

    Register Test code

---
+++ blog-client/src/components/register/__test__/RegisterForm.test.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import RegisterForm,{  RegisterFormProps} from '../RegisterForm';
+import { StaticRouter, MemoryRouter } from 'react-router';
+
+describe('RegisterForm', () => {
+   const setup = (props: Partial<RegisterFormProps> = {}) => {
+     const initialProps: RegisterFormProps = {
+          onSubmit: () => {},
+          defaultEmail: null,
+        };
+        const utils = render(
+            <MemoryRouter>
+              <RegisterForm {...initialProps} {...props} />
+            </MemoryRouter>,
+          );
+          const form = {
+            displayName: utils.getByPlaceholderText(/이름을/) as HTMLInputElement,
+            email: utils.getByPlaceholderText(/이메일을/) as HTMLInputElement,
+            username: utils.getByPlaceholderText(/아이디를/) as HTMLInputElement,
+            shortBio: utils.getByPlaceholderText(/당신을/) as HTMLInputElement,
+          };
+        const changeInputs = () => {
+            fireEvent.change(form.displayName, {
+                target: {
+                    value: '송민석',
+                },
+            });
+            fireEvent.change(form.email, {
+                target: {
+                    value: 'somony9292@gmail.com',
+                },
+            });
+            fireEvent.change(form.username, {
+                target: {
+                    value: 'songc',
+                },
+            });
+            fireEvent.change(form.shortBio, {
+                target: {
+                    value: '안녕하세요',
+                },
+            });
+
+        }
+        return {
+            ...utils,
+            form,
+            changeInputs,
+        }
+   };
+   it('renders property', () => {
+       setup();
+   });
+   it('matches snapshot', () => {
+    const { container } = setup();
+    expect(container).toMatchSnapshot();
+   });
+
+   it('changes input', () => {
+    const utils = setup();
+    const { form, getByValue, changeInputs } = utils;
+    changeInputs();
+    getByValue('송민석');
+    getByValue('somony9292@gmail.com');
+    getByValue('songc');
+    getByValue('안녕하세요');
+  });
+    it('submits form', () => {
+        const onSubmit = jest.fn();
+        const utils = setup({
+          onSubmit,
+        });
+        const { getByText } = utils;
+        utils.changeInputs();
+        const button = getByText('다음');
+        fireEvent.click(button);
+        // expect(onSubmit).toBeCalledWith({
+        //   displayName: '송민석',
+        //   email: 'somony9292@gmail.com',
+        //   username: 'songc',
+        //   shortBio: '안녕하세요',
+        // });
+      });
+});
