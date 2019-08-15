cc4a645cf4ac261dacfce0db73c6cae30ae2dda2
s-ong-c <somony9292@gmail.com>
2019-08-15 11:42:53 +0900

    Register Page

---
+++ blog-client/src/pages/RegisterPage.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import RegisterTemplate from '../components/register/RegisterTemplate';
+
+interface RegisterPageProps{}
+
+const RegisterPage: React.SFC<RegisterPageProps> = props => {
+  return <RegisterTemplate />;
+  };
+
+export default RegisterPage;
