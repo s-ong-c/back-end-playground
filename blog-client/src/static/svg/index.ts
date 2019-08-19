f3582104a659250c59370548ac5d45c618bdb9e9
s-ong-c <somony9292@gmail.com>
2019-08-19 22:25:27 +0900

    register logo create

--- blog-client/src/static/svg/index.ts
+++ blog-client/src/static/svg/index.ts
@@ -2,3 +2,4 @@ export { ReactComponent as Logo} from './logo.svg';
 export { ReactComponent as FacebookIcon} from './facebook.svg';
 export { ReactComponent as GoogleIcon} from './google.svg';
 export { ReactComponent as GithubIcon} from './github.svg';
+export { ReactComponent as Register} from './register.svg';
