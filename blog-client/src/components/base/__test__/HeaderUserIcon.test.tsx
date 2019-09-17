2899600dae6acfc47bb6996d66d33a8525e3d828
s-ong-c <somony9292@gmail.com>
2019-09-17 17:41:04 +0900

    write test code

--- blog-client/src/components/base/__test__/HeaderUserIcon.test.tsx
+++ blog-client/src/components/base/__test__/HeaderUserIcon.test.tsx
@@ -1,27 +1,28 @@
 import * as React from 'react';
 import { render } from 'react-testing-library';
-import HeaderUserIcon,{  HeaderUserIconProps} from '../HeaderUserIcon';
+import HeaderUserIcon, { HeaderUserIconProps } from '../HeaderUserIcon';
 
 describe('HeaderUserIcon', () => {
-   const setup = (props: Partial<HeaderUserIconProps> = {}) => {
-     const initialProps: HeaderUserIconProps = {
-         user : {
-             id: '',
-             username: '',
-             profile: {
-                 thumbnail: '',
-                 display_name: 'test'
-             }
-         }
-        };
-       const utils =  render(<HeaderUserIcon {...initialProps} {...props} />);
-       return {...utils};
-   };
-   it('renders property', () => {
-       setup();
-   });
-   it('matches snapshot', () => {
+  const setup = (props: Partial<HeaderUserIconProps> = {}) => {
+    const initialProps: HeaderUserIconProps = {
+      user: {
+        id: '',
+        username: '',
+        profile: {
+          thumbnail: '',
+          display_name: 'test',
+        },
+      },
+      onClick: () => undefined,
+    };
+    const utils = render(<HeaderUserIcon {...initialProps} {...props} />);
+    return { ...utils };
+  };
+  it('renders property', () => {
+    setup();
+  });
+  it('matches snapshot', () => {
     const { container } = setup();
     expect(container).toMatchSnapshot();
-   })
- });
+  });
+});
---
+++ blog-client/src/modules/__tests__/write.test.ts
@@ -0,0 +1,48 @@
+import * as write from '../write';
+
+const reducer = write.default;
+
+describe('write redux module', () => {
+  const getInitialState = () => reducer(undefined, {});
+  it('should return initialState', () => {
+    const state = getInitialState();
+    expect(state).toEqual({
+      markdown: '',
+      title: '',
+      mode: 'WYSIWYG',
+      html: '',
+      tags: [],
+    });
+  });
+  describe('action handlers', () => {
+    it('CHANGE_MARKDOWN', () => {
+      let state = getInitialState();
+      state = reducer(state, write.changeMarkdown('Hello'));
+      expect(state.markdown).toBe('Hello');
+    });
+    it('CHANGE_TITLE', () => {
+      let state = getInitialState();
+      state = reducer(state, write.changeTitle('World'));
+      expect(state.title).toBe('World');
+    });
+    it('CONVERT_EDITOR_MODE', () => {
+      let state = getInitialState();
+      state = reducer(state, write.convertEditorMode());
+      expect(state.mode).toBe('MARKDOWN');
+      state = reducer(state, write.convertEditorMode());
+      expect(state.mode).toBe('WYSIWYG');
+    });
+    it('SET_HTML', () => {
+      let state = getInitialState();
+      const html = '<p>Hello World</p>';
+      state = reducer(state, write.setHtml(html));
+      expect(state.html).toBe(html);
+    });
+    it('CHANGE_TAGS', () => {
+      let state = getInitialState();
+      const nextTags = ['태그1', '태그2'];
+      state = reducer(state, write.changeTags(nextTags));
+      expect(state.tags).toBe(nextTags);
+    });
+  });
+});
