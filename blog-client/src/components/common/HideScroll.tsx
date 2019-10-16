3c7924154fd17fada40e33a73c46f4c473c25756
s-ong-c <somony9292@gmail.com>
2019-10-16 22:24:31 +0900

    useLayoutEffect .. use

--- blog-client/src/components/common/HideScroll.tsx
+++ blog-client/src/components/common/HideScroll.tsx
@@ -2,15 +2,43 @@ import * as React from 'react';
 
 export interface HideScrollProps {}
 
-const { useEffect } = React;
+const { useEffect, useLayoutEffect } = React;
 /**
  *
  * Hides body scrollbar on mount
  * Revert on unmount
  *
  */
+
+/**
+ * class 작성시
+ */
+// class HideScroll extends React.Component<HideScrollProps> {
+//   prevStyles: CSSStyleDeclaration | null = null;
+
+//   constructor(props: HideScrollProps) {
+//     super(props);
+//     if (typeof window === 'undefined') return;
+//     this.prevStyles = getComputedStyle(window.document.body);
+//   }
+
+//   componentWillMount() {
+//     if (typeof window === 'undefined' || !this.prevStyles) return;
+//     window.document.body.style.overflowX = this.prevStyles.overflowX;
+//     window.document.body.style.overflowY = this.prevStyles.overflowY;
+//   }
+//   render() {
+//     return null;
+//   }
+// }
+
+/**
+ *
+ *  useLayoutEffect :components 가 나타나기전에
+ *                   반영이 되는 componentWillMount() 과 같은역할을 한다
+ */
 const HideScroll: React.SFC<HideScrollProps> = props => {
-  useEffect(() => {
+  useLayoutEffect(() => {
     const prevStyles = getComputedStyle(window.document.body);
     window.document.body.style.overflowX = 'hidden';
     window.document.body.style.overflowY = 'hidden';
