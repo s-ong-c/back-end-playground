86b459f5fedc3493720eb9bc23665381d11df235
s-ong-c <somony9292@gmail.com>
2019-11-17 23:04:57 +0900

    Update Utils code and test utils

---
+++ blog-client/src/lib/__test__/utils.test.ts
@@ -0,0 +1,20 @@
+import { escapeForUrl } from '../utils';
+
+describe('utils', () => {
+  it('escapeForUrl', () => {
+    const samples = [
+      '한글로된 텍스트 사이에 스페이스',
+      'title with English',
+      'title-with-%-special-characters',
+      '   trim    ',
+    ];
+    const escaped = samples.map(escapeForUrl);
+    expect(escaped).toEqual([
+      '한글로된-텍스트-사이에-스페이스',
+      'title-with-English',
+      'title-with-special-characters',
+      'trim',
+    ]);
+    console.log(escaped);
+  });
+});
