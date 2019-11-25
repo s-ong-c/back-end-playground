b47ff8ef6600fbafd3bd9cf3f8539f5c1d85f520
s-ong-c <somony9292@gmail.com>
2019-11-25 22:43:07 +0900

    ramda add , post graphql writePost

--- blog-client/src/lib/graphql/post.ts
+++ blog-client/src/lib/graphql/post.ts
@@ -2,41 +2,42 @@ import gql from 'graphql-tag';
 import { User } from './user';
 
 export type Post = {
-    id: string;
-    title: string;
-    body: string;
-    thumbnail: string;
-    is_markdown: boolean;
-    is_temp: boolean;
-    user: any;
-    url_slug: string;
-    likes: number;
-    meta: any;
-    views: number;
-    is_private: boolean;
-    released_at: string;
-    created_at: string;
-    updated_at: string;
-    short_description: string;
-    comments: [any];
-    tags: [string];
-    comments_count: number;
-  };
-  
-  // Post Type for post list
-  export type PartialPost = {
-    id: string;
-    title: string;
-    short_description: string;
-    thumbnail: string;
-    user: User;
-    url_slug: string;
-    is_private: boolean;
-    released_at: string;
-    tags: [string];
-    comments_count: number;
-  }
-  export const GET_POST_LIST = gql`
+  id: string;
+  title: string;
+  body: string;
+  thumbnail: string;
+  is_markdown: boolean;
+  is_temp: boolean;
+  user: any;
+  url_slug: string;
+  likes: number;
+  meta: any;
+  views: number;
+  is_private: boolean;
+  released_at: string;
+  created_at: string;
+  updated_at: string;
+  short_description: string;
+  comments: [any];
+  tags: [string];
+  comments_count: number;
+};
+
+// Post Type for PostList
+export type PartialPost = {
+  id: string;
+  title: string;
+  short_description: string;
+  thumbnail: string;
+  user: User;
+  url_slug: string;
+  is_private: boolean;
+  released_at: string;
+  tags: [string];
+  comments_count: number;
+};
+
+export const GET_POST_LIST = gql`
   query Post($cursor: ID) {
     posts(cursor: $cursor) {
       id
@@ -55,6 +56,46 @@ export type Post = {
       comments_count
       tags
       is_private
+    }
+  }
+`;
+
+export const WRITE_POST = gql`
+  mutation WritePost(
+    $title: String
+    $body: String
+    $tags: [String]
+    $is_markdown: Boolean
+    $is_temp: Boolean
+    $url_slug: String
+    $thumbnail: String
+    $meta: JSON
+  ) {
+    writePost(
+      title: $title
+      body: $body
+      tags: $tags
+      is_markdown: $is_markdown
+      is_temp: $is_temp
+      url_slug: $url_slug
+      thumbnail: $thumbnail
+      meta: $meta
+    ) {
+      id
+      title
+      body
+      thumbnail
+      is_markdown
+      is_temp
+      user {
+        id
+        username
+        profile {
+          thumbnail
+        }
+      }
+      url_slug
+      meta
     }
   }
 `;
