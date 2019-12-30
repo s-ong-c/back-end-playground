54d9203d966516f3cc411cee0c42996ef821527c
s-ong-c <somony9292@gmail.com>
2019-12-30 21:01:22 +0900

    action type => any

--- blog-client/src/lib/utils.ts
+++ blog-client/src/lib/utils.ts
@@ -1,5 +1,3 @@
-import { AnyAction } from 'redux';
-
 export const getScrollTop = () => {
   if (!document.body) return 0;
   const scrollTop = document.documentElement
@@ -21,7 +19,7 @@ export type Handlers<T> = {
 };
 
 export function createReducer<S>(handlers: Handlers<S>, initialState: S) {
-  return (state: S = initialState, action: AnyAction) => {
+  return (state: S = initialState, action: any) => {
     const handler = handlers[action.type];
     if (!handler) return state;
     return handler(state, action);
