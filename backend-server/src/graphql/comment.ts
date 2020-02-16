b9089896f18d43b0112b638b680d78db56bea478
s-ong-c <somony9292@gmail.com>
2020-02-16 19:36:52 +0900

    writeComment Mutations

--- backend-server/src/graphql/comment.ts
+++ backend-server/src/graphql/comment.ts
@@ -1,7 +1,8 @@
 import { ApolloContext } from './../app';
-import { gql, IResolvers } from 'apollo-server-koa';
+import { gql, IResolvers, AuthenticationError, ApolloError } from 'apollo-server-koa';
 import Comment from '../entity/Comment';
 import { getRepository } from 'typeorm';
+import Post from '../entity/Post';
 
 export const typeDef = gql`
   type Comment {
@@ -18,8 +19,16 @@ export const typeDef = gql`
   extend type Query {
     subcomments(comment_id: ID): [Comment]
   }
-`;
 
+  extend type Mutation {
+    writeComment(post_id: ID!, text: String!, comment_id: ID): Comment
+  }
+`;
+type WriteCommentArgs = {
+  post_id: string;
+  text: string;
+  comment_id?: string;
+};
 export const resolvers: IResolvers<any, ApolloContext> = {
   Comment: {
     text: (parent: Comment) => {
@@ -46,6 +55,44 @@ export const resolvers: IResolvers<any, ApolloContext> = {
         }
       });
       return comments;
+    }
+  },
+  Mutation: {
+    writeComment: async (parent: any, args, ctx) => {
+      if (!ctx.user_id) {
+        throw new AuthenticationError('Not Logged In');
+      }
+
+      const { post_id, comment_id, text } = args as WriteCommentArgs;
+      const post = await getRepository(Post).findOne(post_id);
+      if (!post) {
+        throw new ApolloError('Post not found', 'NOT_FOUND');
+      }
+      const commentRepo = getRepository(Comment);
+      const comment = new Comment();
+
+      if (comment_id) {
+        const commentTarget = await commentRepo.findOne(comment_id);
+        if (!commentTarget) {
+          throw new ApolloError('Target comment is not found', 'NOT_FOUND');
+        }
+        comment.level = commentTarget.level + 1;
+        comment.reply_to = comment_id;
+
+        if (comment.level === 4) {
+          throw new ApolloError('Maximum comment level is 3', 'BAD_REQUEST');
+        }
+
+        commentTarget.has_replies = true;
+        await commentRepo.save(commentTarget);
+      }
+
+      comment.fk_user_id = ctx.user_id;
+      comment.text = text;
+      comment.fk_post_id = post_id;
+
+      await commentRepo.save(comment);
+      return comment;
     }
   }
 };
